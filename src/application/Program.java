72c7ab996798df6312b26271425457eec6fc9757
Evandro Duarte <evandro.duarte@live.com>
2019-04-24 14:55:13 -0400

    program after interface methods

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import services.BrazilInterestService;
+import services.InterestService;
 import services.USAInterestService;
 
 public class Program {
@@ -17,9 +18,9 @@ public class Program {
 		System.out.print("Months: ");
 		int months = sc.nextInt();
 		
-		BrazilInterestService is = new BrazilInterestService(2.0);
+		InterestService is = new BrazilInterestService(2.0);
 		double payment = is.payment(amount, months);
-		USAInterestService is2 = new USAInterestService(1.0);
+		InterestService is2 = new USAInterestService(1.0);
 		double payment2 = is2.payment(amount, months);
 		
 		System.out.println("Payment after " + months + " months:");
